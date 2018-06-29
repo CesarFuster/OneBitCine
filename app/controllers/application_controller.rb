0ce3f878a71f8979b90b8e22eb0c820b1ce82550
cesarfuster <cfuster@uol.com.br>
2018-06-29 19:58:49 -0300

    Languages update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   def set_locale
     session[:locale] = params[:locale] if params[:locale].present?
     I18n.locale = session[:locale] || I18n.default_locale
- end
+  end
 
 end
